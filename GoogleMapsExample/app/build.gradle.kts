644415c2496910c068639fc57f8ab7b80fcd5dbf
Andrés Muñoz O. <andmunoz@gmail.com>
2025-09-23 21:09:33 -0300

    Google Maps API Example (basic)

--- GoogleMapsExample/app/build.gradle.kts
+++ GoogleMapsExample/app/build.gradle.kts
@@ -2,15 +2,14 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kapt)
 }
 
 android {
-    namespace = "cl.uchile.postgrado.mobile.contactlist"
+    namespace = "cl.uchile.postgrado.mobile.googlemapsexample"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "cl.uchile.postgrado.mobile.contactlist"
+        applicationId = "cl.uchile.postgrado.mobile.googlemapsexample"
         minSdk = 30
         targetSdk = 36
         versionCode = 1
@@ -41,33 +40,22 @@ android {
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.room.common.jvm)
-    implementation(libs.androidx.room.ktx)
-    implementation(libs.androidx.room.runtime)
-    implementation(libs.material3)
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.runtime.livedata)
-    implementation(libs.retrofit)
-    implementation(libs.retrofit.converter.gson)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
     implementation(libs.maps.compose)
     implementation(libs.play.services.maps)
-
-    kapt(libs.androidx.room.compiler)
-
     testImplementation(libs.junit)
-
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
